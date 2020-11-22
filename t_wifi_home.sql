f11a6463e2533447b834af93a8d06e9d7f6e6f44
codewindy <bravohex@gmail.com>
2020-11-23 00:15:10 +0800

    add t_wifi_home.sql

---
+++ t_wifi_home.sql
@@ -0,0 +1,5 @@
+INSERT INTO `t_wifi_home`(`id`, `ssid`, `mac`, `type`, `passwd`, `alias`, `pppoe`, `router_pwd`) VALUES (1, 'zfy', '2C:B2:1A:CB:9A:21', 'wpa2', '88888888', '老家移动', NULL, 'a1234567');
+INSERT INTO `t_wifi_home`(`id`, `ssid`, `mac`, `type`, `passwd`, `alias`, `pppoe`, `router_pwd`) VALUES (2, '@PHICOMM_44_5G', '68:DB:54:E7:20:46', 'wpa2', 'zlx19950809', '炮鸡窝移动', '18897963059', '');
+INSERT INTO `t_wifi_home`(`id`, `ssid`, `mac`, `type`, `passwd`, `alias`, `pppoe`, `router_pwd`) VALUES (3, 'TP-LINK_F4E0', 'F4:83:CD:7A:F4:E0', 'wpa2', 'ZHOUYUCHENG18070461152', '木生里买电信', NULL, 'a1234567');
+INSERT INTO `t_wifi_home`(`id`, `ssid`, `mac`, `type`, `passwd`, `alias`, `pppoe`, `router_pwd`) VALUES (4, 'TP-LINK_2F85', 'B0:95:8E:E9:2F:85', 'wpa', 'zhf830610', '阿罗仔电信', '', 'a1234567');
+INSERT INTO `t_wifi_home`(`id`, `ssid`, `mac`, `type`, `passwd`, `alias`, `pppoe`, `router_pwd`) VALUES (5, 'TP-LINK_D1C9', '9C:A6:15:85:D1:C9', 'wpa', '19880522', '讨米个家朵', '', 'a1234567');
